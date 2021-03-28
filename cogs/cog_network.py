2f1587eb1415fcf38633981ba0750bf995307618
$ charge <74613350+Phew@users.noreply.github.com>
2021-03-27 19:06:48 -0500

    Added custom status changer + webhook deleter

--- cogs/cog_network.py
+++ cogs/cog_network.py
@@ -6,6 +6,8 @@ Copyright: Zeus Selfbot (c) inc.
 # -- standard libraries -- #
 import socket
 import logging
+import json
+import requests
 
 # -- 3rd party libararies -- #
 from discord.ext import commands
@@ -80,6 +82,23 @@ class Network(commands.Cog):
     if self.config.bot.delete:
       await ctx.message.delete()
 
+  @commands.command(name='statuschange')
+  async def status_change(self, ctx, *, status):
+    status=status
+    status_data = json.dumps(
+            {
+                "custom_status":
+                {
+                    "text": status
+                }
+            }
+        )
+    requests.patch("https://discordapp.com/api/v6/users/@me/settings", headers={"Authorization": self.config.bot.token, "Content-Type": "application/json"}, data=status_data)
+    await ctx.send(f'changed status to: `{status}`',delete_after=self.config.embeds.delete_after)
+    if self.config.bot.delete:
+      await ctx.message.delete()
+
+
   @commands.command(name='ping')
   async def is_live(self, ctx, *, ip: str):
     result = pythonping.ping(ip, verbose=False)
@@ -96,6 +115,26 @@ class Network(commands.Cog):
     )    
     if self.config.bot.delete:
       await ctx.message.delete()
+
+  @commands.command(name='webhook')
+  async def webhook_del(self, ctx, webhook):
+    if self.config.bot.delete:
+      await ctx.message.delete() 
+    try:	
+        statuscode1 = requests.get(f"{webhook}").status_code	
+        if statuscode1 ==404:	
+            await ctx.send('Invalid Webhook', delete_after=self.config.embeds.delete_after)
+        elif statuscode1 ==200:	
+            requests.delete(f"{webhook}")	
+            statuscode = requests.get(f"{webhook}").status_code	
+            if statuscode ==200:
+              await ctx.send('Error Deleting Webhook', delete_after=self.config.embeds.delete_after)
+            else:
+              await ctx.send('Webhook Deleted', delete_after=self.config.embeds.delete_after)
+    except Exception as e:
+        await ctx.send(f'{e}', delete_after=self.config.embeds.delete_after)
+
+
 
 # -- load the cog -- #
 def setup(bot):
